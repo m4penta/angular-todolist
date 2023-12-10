e14e8270ab064dda0a8b33fcb8e52c5f713cf9f1
m4penta <80562619+m4penta@users.noreply.github.com>
2023-12-10 22:26:59 +0700

    create json server

--- src/app/services/localData.ts.service.ts
+++ src/app/services/localData.ts.service.ts
@@ -1,34 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { Todo } from '../models/todo';
 @Injectable({
   providedIn: 'root'
 })
 export class LocalDataServices {
 
-constructor() {}
+constructor() {
+
+}
 
 
 
-  // Lưu trữ dữ liệu vào Local Storage
-  setItem(key: string, value: any): void {
-    localStorage.setItem(key, JSON.stringify(value));
-  }
-
-  // Truy xuất dữ liệu từ Local Storage
-  getItem(key: string): any {
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : null;
-  }
-
-  // Xóa mục từ Local Storage
-  removeItem(key: string): void {
-    localStorage.removeItem(key);
-  }
-
-  // Xóa tất cả dữ liệu trong Local Storage
-  clear(): void {
-    localStorage.clear();
 
 
-
-}}
+}
